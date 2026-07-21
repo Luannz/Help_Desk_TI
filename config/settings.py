fa511635932b6c40e0fdd6448f4e88d23b735ba7
Luannz <luanncc2014@gmail.com>
2026-07-21 15:48:39 -0300

    ajutando o debug

--- config/settings.py
+++ config/settings.py
@@ -28,15 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ctw@s+%5#sk&li-d+#ml(kus!9)jrv$o45721#!rgz=m8*x226')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     '*',
     'localhost',
     '127.0.0.1',
     '192.168.2.4',
+    '192.168.2.244',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
